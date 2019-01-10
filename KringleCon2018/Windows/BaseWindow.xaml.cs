515aa0af26de4a5cea96e12f7aef614a9a6591a0
djengineerllc <phillipmoore@gmail.com>
2019-01-09 22:48:16 -0600

    Update styles, add background music

--- KringleCon2018/Windows/BaseWindow.xaml.cs
+++ KringleCon2018/Windows/BaseWindow.xaml.cs
@@ -1,13 +1,58 @@
-﻿namespace KringleCon2018
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+
+namespace KringleCon2018
 {
     /// <summary>
     /// Interaction logic for BaseWindow.xaml
     /// </summary>
     public partial class BaseWindow
     {
+        public List<string> Songs = new List<string> {
+            "Brandenburgh #3",
+            "Christmas in Hollis",
+            "Dreidel",
+            "Jingle Bells",
+            "Let it Snow"
+        };
         /// <summary>
         /// Initializes a new instance of the <see cref="BaseWindow"/> class
         /// </summary>
-        public BaseWindow() => InitializeComponent();
+        public BaseWindow()
+        {
+            InitializeComponent();
+            PlaySound(Songs[0]);
+        }
+        public void PlaySound(string song, Action done = null)
+        {
+            var ms = File.OpenRead($@"{Environment.CurrentDirectory}/Resources/Songs/{song}.mp3");
+            var rdr = new Mp3FileReader(ms);
+            var wavStream = WaveFormatConversionStream.CreatePcmStream(rdr);
+            var baStream = new BlockAlignReductionStream(wavStream);
+            var waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
+            waveOut.Init(baStream);
+            waveOut.Play();
+            var bw = new BackgroundWorker();            
+            bw.DoWork += (s, o) => {
+                while (waveOut.PlaybackState == PlaybackState.Playing) { Thread.Sleep(100); }
+                waveOut.Dispose();
+                baStream.Dispose();
+                wavStream.Dispose();
+                rdr.Dispose();
+                ms.Dispose();
+                done?.Invoke();
+                o.Result = o.Argument;
+            };
+            bw.RunWorkerAsync(song);
+            bw.RunWorkerCompleted += ((s, o) => {
+                var nextSong = Songs.IndexOf((string)o.Result) + 1;
+                if (nextSong > (Songs.Count - 1)) { nextSong = 0; }
+                PlaySound(Songs[nextSong]);
+            });
+        }
     }
 }
--- KringleCon2018/obj/Debug/KringleCon2018_Content.g.i.cs
+++ KringleCon2018/obj/Debug/KringleCon2018_Content.g.i.cs
@@ -8,6 +8,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/songs/brandenburgh%20%233.mp3")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/songs/christmas%20in%20hollis.mp3")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/songs/dreidel.mp3")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/songs/jingle%20bells.mp3")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/songs/let%20it%20snow.mp3")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/songs/ode%20to%20joy.mp3")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/songs/winter%20wonderland.mp3")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/objectives/objective8/app.js")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/objectives/objective8/index.html")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/objectives/objective8/login.html")]
